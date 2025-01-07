409dd50e816503dd2aeec9d1b1c0a0b8bde09817
Krissmes <kriss.iesalnieks@gmail.com>
2025-01-07 12:34:06 +0200

    a
    a

---
+++ saistitais_saraksts/klases.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,saturs, pirms=None, pec=None):
+        self.info = saturs
+        self.next = pec
+        self.prev = pirms
+
+    def read(self):
+        print(self.info)
+
+
+class List:
+    def __init__(self):
+        self.sakums = None
+
+    def add(self, jaunais):
+        if self.sakums == None:
+            self.sakums = Node(jaunais)
+        else:
+            pedejais = self.sakums
+            while pedejais.next:
+                pedejais = pedejais.next
+            pedejais.next = Node(jaunais, pirms=pedejais)
+            
+    def read(self):
+        if self.sakums == None:
+            print("Saraksts ir tukšs")
+        esosais = self.sakums
+        
+        while esosais:
+            esosais.read()
+            esosais = esosais.next
+            
+
+saraksts = List()
+saraksts.add("kakis")
+saraksts.add("suns")
+saraksts.add("maja")
+saraksts.add("zogs")
+saraksts.add("stabs")
+
+
+saraksts.read()
+
+saraksts.sakums.read()
+print("=====")
+saraksts.sakums.read()
