9db97451e696cea212b14b016c81035ff714165f
Krissmes <kriss.iesalnieks@gmail.com>
2025-01-28 10:51:43 +0200

    a
    a

---
+++ masin_macisanas/masinmacisana.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+
+from termcolor import colored as cl
+from sklearn.model_selection import train_test_split 
