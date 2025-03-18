c884b20978a47ed68c6cda0d0d5d5e4eecbec62c
Krissmes <kriss.iesalnieks@gmail.com>
2025-03-18 23:48:13 +0200

    a
    a

--- saistitais_saraksts/klases.py
+++ saistitais_saraksts/klases.py
@@ -66,20 +66,71 @@ class List:
         return
     def pop_cool(self, indekss):
         print("majasdarbs")
+        if self.skaits == 0:
+            print("Nav ko dzest!")
+            return
+        if self.skaits < indekss:
+            print("nav tads elements")
+            return
+        esosais = self.sakums
+        for x in range(indekss):
+            esosais = esosais.next
+        galva =  esosais.prev
+        aste = esosais.next
+        galva.next = aste
+        aste.prev = galva
+        self.skaits -= 1
+        esosais = None
 
     def switch(self, indekss1, indekss2):
-        elements1 = self.sakums
+        if self.skaits < 1:
+            print("Nav ko mainit")
+            return
+        if self.skaits < indekss1 or self.skaits < indekss2:
+            print("nav tads elements")
+            return
+        pirmais = self.sakums
         for x in range(indekss1):
-            elements1 = elements1.next
-            print(elements1)
-        
-        elements2 = self.sakums
-        for x in range(indekss2):
-            elements2 = elements2.next
-            print(elements2)
+            pirmais = pirmais.next
+        otrais = self.sakums
+        for y in range(indekss2):
+            otrais = otrais.next
 
-        elements1.info, elements2.info = elements2.info, elements1.info
+        temp_n = pirmais.next
+        temp_p = pirmais.prev
+        pirmais.next = otrais.next
+        pirmais.prev = otrais.prev
+        otrais.next = temp_n
+        otrais.prev = temp_p
         
+        # temp_p_p = self.sakums
+        # for z in range(indekss1 - 1):
+        #     temp_p_p = temp_p_p.next
+
+        # temp_p_n = self.sakums
+        # for z in range(indekss1 + 1):
+        #     temp_p_n = temp_p_n.next
+
+        # temp_o_p = self.sakums
+        # for z in range(indekss2 - 1):
+        #     temp_o_p = temp_o_p.next
+
+        # temp_o_n = self.sakums
+        # for z in range(indekss2 + 1):
+        #     temp_o_n = temp_o_n.next
+
+
+        # temp_p_p.next = otrais
+        # temp_p_n.prev = otrais
+        # temp_o_p.next = pirmais
+        # temp_o_n.prev = pirmais
+
+        # temp = pirmais.prev.next
+        # temp = pirmais.next.prev
+        # pirmais.prev.next = otrais
+        # pirmais.next.prev = otrais
+        # otrais.prev.next = temp
+        # otrais.next.prev = temp
 
 
 
@@ -92,14 +143,19 @@ saraksts.add("suns")
 saraksts.add("maja")
 saraksts.add("zogs")
 saraksts.add("stabs")
+saraksts.add("loks")
+
+saraksts.read()
+print("--------")
+saraksts.switch(1,2)
 
 
 saraksts.read()
 
-saraksts.sakums.read()
-print("=====")
-saraksts.sakums.read()
+# saraksts.sakums.read()
+# print("=====")
+# saraksts.sakums.read()
 
-saraksts.switch(1,4)
 
-saraksts.read()
+
+# saraksts.read()
